0f3523ba6816791f074cbe73f30efa9de6091d00
Josh Schonstal <josh@schonstal.org>
2021-01-30 15:55:49 -0800

    🎮 Add robot flassh

---
+++ Enemies/Body.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+onready var flash_tween = $FlashTween
+
+func _ready():
+  EventBus.connect("enemy_damage", self, "_on_enemy_damage")
+  
+func _on_enemy_damage(_data:Dictionary):
+  flash_tween.stop_all()
+  flash_tween.interpolate_property(
+    self,
+    "modulate",
+    Color(0.7, 0.5, 0.7, 1),
+    Color(1, 1, 1, 1),
+    0.1,
+    Tween.TRANS_QUART,
+    Tween.EASE_IN
+  )
+  flash_tween.start()
+  yield(flash_tween, "tween_completed")
+  flash_tween.interpolate_property(
+    self,
+    "modulate",
+    Color(3, 3, 3, 1),
+    Color(1, 1, 1, 1),
+    0.1,
+    Tween.TRANS_QUART,
+    Tween.EASE_IN
+  )
+  flash_tween.start()
